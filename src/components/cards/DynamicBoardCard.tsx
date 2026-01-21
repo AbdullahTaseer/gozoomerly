95eb10cf550aa9c7425930b4ab9f67569f83d9a9
Abdullah Taseer <itsabdullahtaseer@gmail.com>
2026-01-21 13:27:57 +0500

    adjust board card heading

--- src/components/cards/DynamicBoardCard.tsx
+++ src/components/cards/DynamicBoardCard.tsx
@@ -93,7 +93,7 @@ const DynamicBoardCard: React.FC<DynamicBoardCardProps> = ({
         </div>
 
         <div className="relative z-10">
-          <h3 className="text-[28px] font-bold mb-4 line-clamp-1 overflow-hidden text-ellipsis">{title}</h3>
+          <h3 className="text-[20px] font-bold mb-4 line-clamp-1 overflow-hidden text-ellipsis">{title}</h3>
 
           <div className="flex items-start gap-3 mb-4">
             <div className="relative h-14 w-14 shrink-0 z-10">
